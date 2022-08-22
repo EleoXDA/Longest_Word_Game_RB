fb9899b37039f8db4ccd570d5eab2eefa9eae192
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 22:34:15 +0200

    ADD: game logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,45 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = Array.new(5) { %w[A E I O U Y].sample }
     @letters += Array.new(5) { (('A'..'Z').to_a - %w[A E I O U Y]).sample }
+    @score = session[:score]
+  end
+
+  def store_score(score)
+    session[:score].nil? ? session[:score] = 0 : session[:score] += score
+  end
+
+  def remove_score
+    session.delete(:score)
   end
 
   def score
-    raise
+    letters = params[:letters]
+    answer = params[:answer].split('')
+    check_array = []
+    score = 0
+
+    answer.each do |a|
+      if letters.include?(a)
+        check_array.push(a)
+      end
+    end
+
+    url = "https://wagon-dictionary.herokuapp.com/#{answer.join}"
+    parse = JSON.parse(URI.open(url).read)
+    check = parse['found']
+
+    if check_array.sort == answer.sort && check == true
+      @output = 'Congratulations! You win'
+      score = answer.length
+    elsif check == false
+      @output = 'WORD is not in the dictionary'
+    elsif check_array.sort != answer.sort
+      @output = 'WORD does not contain required letters'
+    end
+
+    store_score(score)
   end
 end

ac607c0dc6d899e55be5f958859390c69d8d2748
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 21:33:48 +0200

    ADD: route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'new', to: 'games#new', as: :new
+  get 'score', to: 'games#score', as: :score
 end

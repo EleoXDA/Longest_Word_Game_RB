90e0ed277c0febfe792eeee1d943fb5eb3ab35a5
Elbay Malikmammadov <eleo.666@gmail.com>
2022-08-22 22:19:46 +0200

    ADD: letters logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,7 @@
 class GamesController < ApplicationController
   def new
-    @letters
+    @letters = Array.new(5) { %w[A E I O U Y].sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - %w[A E I O U Y]).sample }
   end
 
   def score
